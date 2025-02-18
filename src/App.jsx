daef861fc54b924c30d7dd6b2f0e4862ef3ddacd
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2025-02-18 22:51:35 +0530

    data

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/articles' element={<Articles/>}/>
+        <Route path='/articles/:userName' element={<Articles/>}/>
       </Routes>
     </div>
   );
---
+++ src/data/users.js
@@ -0,0 +1,85 @@
+export const UserDetails = [
+    [
+        {
+          "name": "Kai Thorne",
+          "username": "kaithorne_author",
+          "jobRole": "Space Explorer",
+          "experience": "10 years",
+          "company": "Interstellar Ventures",
+          "profileImage": "https://example.com/kaithorne.jpg"
+        },
+        {
+          "name": "Selene Starfall",
+          "username": "selenestarfall_writes",
+          "jobRole": "Dream Architect",
+          "experience": "5 years",
+          "company": "Starfall Studios",
+          "profileImage": "https://example.com/selenestarfall.jpg"
+        },
+        {
+          "name": "Vera Lysandra",
+          "username": "veralysandra_writer",
+          "jobRole": "Time Traveler",
+          "experience": "12 years",
+          "company": "ChronoCorp",
+          "profileImage": "https://example.com/veralysandra.jpg"
+        },
+        {
+          "name": "Orin Blackwood",
+          "username": "orinblackwood_writer",
+          "jobRole": "Mystic Investigator",
+          "experience": "8 years",
+          "company": "Blackwood Investigations",
+          "profileImage": "https://example.com/orinblackwood.jpg"
+        },
+        {
+          "name": "Lyra Moonstone",
+          "username": "lyramoonstone_author",
+          "jobRole": "Elemental Sorceress",
+          "experience": "15 years",
+          "company": "Moonstone Magic Academy",
+          "profileImage": "https://example.com/lyramoonstone.jpg"
+        },
+        {
+          "name": "Caden Drift",
+          "username": "cadendrift_writer",
+          "jobRole": "Underwater Explorer",
+          "experience": "7 years",
+          "company": "Aqua Ventures",
+          "profileImage": "https://example.com/cadendrift.jpg"
+        },
+        {
+          "name": "Elara Skye",
+          "username": "elara_skye_author",
+          "jobRole": "Sky Pirate",
+          "experience": "10 years",
+          "company": "Skybandit Crew",
+          "profileImage": "https://example.com/elaraskye.jpg"
+        },
+        {
+          "name": "Jax Wilde",
+          "username": "jaxwilde_writer",
+          "jobRole": "Wilderness Survival Expert",
+          "experience": "18 years",
+          "company": "Wild Heart Expeditions",
+          "profileImage": "https://example.com/jaxwilde.jpg"
+        },
+        {
+          "name": "Amara Voss",
+          "username": "amaravoss_writer",
+          "jobRole": "Virtual Reality Designer",
+          "experience": "6 years",
+          "company": "Voss VR Studios",
+          "profileImage": "https://example.com/amaravoss.jpg"
+        },
+        {
+          "name": "Raven Ashcroft",
+          "username": "ravenashcroft_author",
+          "jobRole": "Dark Realm Guardian",
+          "experience": "25 years",
+          "company": "Shadow Watchers",
+          "profileImage": "https://example.com/ravenashcroft.jpg"
+        }
+      ]
+      
+]
